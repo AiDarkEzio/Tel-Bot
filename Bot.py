997d3a9aa457cb850ca9a6275a508e002941a0b6
Subadra bro <scienceposhitha@gmail.com>
2022-04-15 07:00:59 -0700

    Bot reCreated: _for test_

--- Bot.py
+++ Bot.py
@@ -1,15 +1,5 @@
-# from telegram import Update
-# from telegram.ext import Updater, CommandHandler, CallbackContext
+import main as Bot
 
-# print("Bot Is starteing")
+pswd = input("Enter Bot password: ")
 
-# def hello(update: Update, context: CallbackContext) -> None:
-#     update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
-
-# updater = Updater('1995592291:AAHciO-rtY7TrdE3e1qVRLcc4UmBxLjBzYw')
-
-# updater.dispatcher.add_handler(CommandHandler('hello', hello))
-
-# updater.start_polling()
-# updater.idle()
+Bot.main(pswd)
--- main.py
+++ main.py
@@ -1,6 +1,53 @@
+from telegram import Update
+from telegram.ext import *
 import Data_Base.Config as Key
-from telegram.exe import *
 import Modules.Respon as Res
 
-print('Bot is started.....')
+def start_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+def help_command(update, context):
+    update.message.reply_text("dfdj324685hrvkkshfefkrekhfs3458734reyfuw")
+    
+def handle_message(update, context):
+    text = str(update.message.text).lower
+    response = Res.sample_responses(text)
+    
+    update.message.reply_text(response)
+    
+def error(update, context):
+    print(f"Update {update} causted error {context.error}")
+    
+def main():
+    updater = Updater(Key.API_KEY, use_context=True)
+    dp = updater.dispatcher
+    
+    dp.add_handler(CommandHandler("start", start_command))
+    dp.add_handler(CommandHandler("start", help_command))
+    dp.add_handler(MessageHandler(Filters.text, handle_message))
+    dp.add_handler(error)
+    
+    updater.start_polling()
+    updater.idle()
+
+main()
+
+###################################################################################################
+
+# def main(password):
+#     if(password == "poshitha"):
+#         updater = Updater(Key.API_KEY, use_context=True)
+#         dp = updater.dispatcher
+        
+#         dp.add_handler(CommandHandler("start", start_command))
+#         dp.add_handler(CommandHandler("start", help_command))
+#         dp.add_handler(MessageHandler(Filters.text, handle_message))
+#         dp.add_handler(error)
+        
+#         updater.start_polling()
+#         updater.idle()
+
+#     else:
+#         return print("Your Password Is Incorret")
+
+###################################################################################################
